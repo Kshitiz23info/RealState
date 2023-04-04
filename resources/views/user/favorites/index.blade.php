13d27ff2eaf2d26591ca086e4695bdd12ed318f1
Kshitiz23info <anupama.bajracharya822@gmail.com>
2023-04-04 21:54:35 +0545

    Fix error in favorites

--- resources/views/user/favorites/index.blade.php
+++ resources/views/user/favorites/index.blade.php
@@ -41,39 +41,10 @@
                                     <span>Area</span>
 
                                 </div>
-                                <span>Type: {{$item->features?json_decode($item->features)->type:'N/a'}}</span><p class="" style="margin: 0.5rem 0">{{$item->description?:'N/A'}}</p>
+                                <span>Type: {{ json_decode($item->listing->features)?json_decode($item->listing->features)->type ?:'N/A':'N/A'}}</span><p class="" style="margin: 0.5rem 0">{{$item->listing->description? Str::limit($item->listing->description, 40) :'N/A'}}</p>
                                 <a href="{{route($route.'show', $item->listing->id)}}" class="btn btn-secondary ">Read More</a>
                             </div>
                         </div>
-                        {{--                            <div class="col-md-3">--}}
-                        {{--                                <div class="card h-100 m-0 p-0">--}}
-                        {{--                                    <div class="img">--}}
-                        {{--                                        <img class="panel-img" src="{{$rentdetail->getMedia('listings')[0]->getFullUrl()}}" alt="Rent Property Image" style="width: 100%; height: 30vh; object-fit: cover">--}}
-                        {{--                                    </div>--}}
-                        {{--                                    <div class="card-body mb-0 pb-0">--}}
-                        {{--                                        <p class="text-sm font-weight-bold m-0 p-0">Rs. {{ $rentdetail->price }}</p>--}}
-                        {{--                                        <p class="text-xs m-0 p-0"><span class="font-weight-bold"> {{ json_decode($rentdetail->features)->bedroom }}</span> beds | <span class="font-weight-bold">{{ json_decode($rentdetail->features)->bathroom }}</span>baths | <span class="font-weight-bold">{{ json_decode($rentdetail->features)->area }}</span>m<sup>2</sup> </p>--}}
-                        {{--                                        <p class="text-xs m-0 p-0">{{ explode(',', $rentdetail->location)[0] . ','. explode(',', $rentdetail->location)[1]. ','. explode(',', $rentdetail->location)[3]}} </p>--}}
-                        {{--                                    </div>--}}
-                        {{--                                    <div class="footer mx-3 mb-3 mt-0">--}}
-                        {{--                                        <div class="d-inline-flex float-left text-xs my-3">--}}
-                        {{--                                            <a href="{{route($route.'show', $rentdetail->id)}}">Click here to view ></a>--}}
-                        {{--                                        </div>--}}
-                        {{--                                        <div class="d-inline-flex float-right">--}}
-                        {{--                                            <a href="{{route($route.'edit', $rentdetail->id)}}" class="btn btn-primary mr-2"><i class="fas fa-fw fa-pencil-alt"></i></a>--}}
-                        {{--                                            <form class="d-inline" action="{{ route($route.'destroy',$rentdetail->id) }}"--}}
-                        {{--                                                  method="POST" onclick="return confirm('Are you sure?')">--}}
-                        {{--                                                @csrf--}}
-                        {{--                                                @method('DELETE')--}}
-                        {{--                                                <button href="{{route($route.'destroy',$rentdetail->id)}}" type="submit" class="btn btn-danger"><i class="fas fa-trash fa-md"></i></button>--}}
-                        {{--                                            </form>--}}
-                        {{--                                        </div>--}}
-                        {{--                                    </div>--}}
-
-
-
-                        {{--                                </div>--}}
-                        {{--                            </div>--}}
                     @endforeach
                     @else
                     <div class="alert alert-info mt-5">
